d2c40630a416f361186ba39d26db9d59874b2d97
Volodymyr892 <sevcukvolodimir896@gmail.com>
2024-11-12 23:03:26 +0200

    set up Tailwind CSS for зкÑ project and set up library сÐ clsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
+import StatusLabel, {Status} from "@/app/components/status-label";
 
 export default function Home() {
   return (
       <main >
-       <h1>Home Pages</h1>
+       <h1 className="text-xl ml-40">Home Pages</h1>
+          <StatusLabel status={Status.Active}>Active</StatusLabel>
+          <StatusLabel status={Status.NotActive}>Not Active</StatusLabel>
+          <StatusLabel status={Status.Pending}>Pending</StatusLabel>
+          <StatusLabel status={Status.Suspended}>Suspended</StatusLabel>
       </main>
   );
 }
