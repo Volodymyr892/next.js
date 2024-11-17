06d7aed4893ac076f672d695d849373dd5f80ab2
Volodymyr892 <sevcukvolodimir896@gmail.com>
2024-11-17 18:55:10 +0200

    СÑcreated button, company form, input,logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import StatusLabel, {Status} from "@/app/components/status-label";
+import { headers } from 'next/headers';
 
 export default function Home() {
   return (
       <main >
-       <h1 className="text-xl ml-40">Home Pages</h1>
+       <h1 className="text-xl ml-40">Home Pages {new Date().toTimeString()}</h1>
           <StatusLabel status={Status.Active}>Active</StatusLabel>
           <StatusLabel status={Status.NotActive}>Not Active</StatusLabel>
           <StatusLabel status={Status.Pending}>Pending</StatusLabel>
