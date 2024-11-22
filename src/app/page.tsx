60ca8400f298dee399696db6e5c805227a606b8b
Volodymyr892 <sevcukvolodimir896@gmail.com>
2024-11-22 13:08:42 +0200

    routing next.js

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,19 @@
 
 import AddCompanyButton from '@/app/components/add-company-button';
+import ServerComponent from '@/app/components/server-component';
+import ClientComponent from '@/app/components/client-component';
+import ServerComponentCopy from '@/app/components/server-component-copy';
+
 
 export default function Home() {
   return (
       <main >
        <h1 className="text-xl ml-40">Home Pages {new Date().toTimeString()}</h1>
          <AddCompanyButton/>
+         <ServerComponent/>
+         <ClientComponent>
+           <ServerComponentCopy/>
+         </ClientComponent>
       </main>
   );
 }
