e0c2b218c9efb308f496b89f2025e21695df19f3
Volodymyr892 <sevcukvolodimir896@gmail.com>
2024-12-06 15:58:04 +0200

    create Intercepting Routes

--- src/app/(admin)/companies/page.tsx
+++ src/app/(admin)/companies/page.tsx
@@ -1,19 +1,10 @@
 import React from 'react';
-import Header from '@/app/components/header';
-import Toolbar from '@/app/components/toolbar';
-import SearchInput from '@/app/components/search-input';
-import AddCompanyButton from '@/app/components/add-company-button';
 import CompanyTablet from '@/app/components/company-tablet';
 import CompanyRow from '@/app/components/company-row';
 import { Status } from '@/app/components/status-label';
 
 export default function Page() {
   return (
-    <>
-      <Header>Companies</Header>
-      <Toolbar action={<AddCompanyButton />}>
-        <SearchInput />
-      </Toolbar>
       <CompanyTablet>
         <CompanyRow
           id={1}
@@ -25,6 +16,5 @@ export default function Page() {
           joinedDate={"02.19.2023"}
         />
       </CompanyTablet>
-    </>
   );
 }
--- src/app/components/add-company-button.tsx
+++ src/app/components/add-company-button.tsx
@@ -1,22 +1,14 @@
 'use client'
-import React, { useState } from 'react';
-import dynamic from 'next/dynamic';
+import React from 'react';
 import Button from './button';
-const CompanyFromModal = dynamic(()=> import("./company-form-modal"),{
-  ssr: false,
-})
+import { useRouter } from 'next/navigation';
+
 
 export default function AddCompanyButton() {
-  const [show, setShow] = useState(false);
+  const router = useRouter();
+
   return (
-    <>
-      <Button onClick={()=> setShow(true)}>Add company</Button>
-      <CompanyFromModal
-        onSubmit={console.log}
-        show={show}
-        onClose={()=> setShow(false)}
-      />
-    </>
+      <Button onClick={()=> router.push('./companies/new')}>Add company</Button>
   );
 }
 
